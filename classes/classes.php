c29d6377cba712d396d31afc9b1c58da71006f36
ieeah <lbnfnc90@gmail.com>
2021-12-22 16:06:25 +0100

    set files

---
+++ classes/classes.php
@@ -0,0 +1,20 @@
+
+<?php
+class Movie
+{
+	public $title;
+	public $year;
+	public $related;
+	public $cast;
+	public $categories;
+}
+
+$Matrix = new Movie();
+$Matrix = new Movie();
+$Matrix = new Movie();
+$Matrix = new Movie();
+$Matrix = new Movie();
+$Matrix = new Movie();
+
+var_dump($Matrix);
+?>
